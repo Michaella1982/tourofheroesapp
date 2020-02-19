9c125c17631842815e3af519cc5d8a214c3b3818
Michaella Babbitt <tinydynamine@gmail.com>
2020-02-19 17:25:41 -0600

    added dashboard and navigation components

--- src/app/hero..service.spec.ts
+++ src/app/hero..service.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
-import { Hero.Service } from './hero..service';
+import { HeroService } from './hero..service';
 
 describe('Hero.Service', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: Hero.Service = TestBed.get(Hero.Service);
+    const service: HeroService = TestBed.get(HeroService);
     expect(service).toBeTruthy();
   });
 });
